5022297616bb3ec5ddea679450eec085b1a3534b
He110te4m <i@he110.info>
2023-04-09 12:12:06 +0800

    refactor(parser): rename parser function name

--- packages/wechat/src/WeChatFeed.ts
+++ packages/wechat/src/WeChatFeed.ts
@@ -1,7 +1,7 @@
 import { BaseFeedParser, type BaseFeedParserOption, type ParsedFeed } from '@feeds/feed'
 
 export class WeChatFeedParser extends BaseFeedParser {
-  public override parser(options: BaseFeedParserOption): ParsedFeed {
-    return super.parser(options)
+  public override parse(options: BaseFeedParserOption): ParsedFeed {
+    return super.parse(options)
   }
 }
